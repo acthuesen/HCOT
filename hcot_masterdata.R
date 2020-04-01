75f76f24d0b265d29d7a385e5a194f734a886a98
acthuesen <37144128+acthuesen@users.noreply.github.com>
2020-04-01 18:03:26 +0200

    Update hcot_masterdata.R

--- hcot_masterdata.R
+++ hcot_masterdata.R
@@ -1,49 +1,52 @@
 #This creates (a) master dataset(s) for HCOT study
+#This version is updated April 2020
 #Ammend as new datasources (tracer etc. is included)
 
-  ### DEPENDENCIES ###
+
+### DEPENDENCIES ###
 library(tidyverse)
-library(readxl)
 library(lubridate)
 library(MESS)
 
 
-	### IMPORT DATA ###
-# various datasources below
-# replace wd with your wd
-# hcot_ap = anthropometry
-hcot_ap <- read.table('wd/HCOT_dag 1_A-C.txt', sep=",", header=TRUE)
+### WORKING DIR ###
+setwd('P:/projects/HCOF/Data/')
+
+
+### IMPORT DATA ###
+# hcot ap = anthropometry
+hcot_ap <- read.table('./ss/HCOT_dag 1_A-C.txt', sep=",", header=TRUE)
 
 # hcot_bw = birth weight
-hcot_bw <- read.table('wd/HCOT_FV.txt', sep=",", header=TRUE)
+hcot_bw <- read.table('./ss/HCOT_FV.txt', sep=",", header=TRUE)
 
 # hcot_tr = training randomization
-hcot_tr <- read.table('wd/HCOT_randomization.txt', sep="\t", header=TRUE)
+hcot_tr <- read.table('./ss/HCOT_randomization.txt', sep="\t", header=TRUE)
 
 # hcot_bd = bone density dexa data
-hcot_bd <- read.table('wd/HCOT_DXA BMD_A-C.txt', sep=",", header=TRUE)
+hcot_bd <- read.table('./ss/HCOT_DXA BMD_A-C.txt', sep=",", header=TRUE)
 
 # hcot_vf = visceral fat mass dexa data
-hcot_vf <- read.table('wd/HCOT_DXA VF_A-C.txt', sep=",", header=TRUE)
-
-# hcot_lf = liver fat data from MRI
-hcot_lf <- read.table('wd/HCOT_MRI_A-C_long.txt', sep=",", header=TRUE)
-
-# hcot_bl = biochemistry for baseline
-hcot_bl <- read_excel('wd/Dag 2 A-C_lab.xlsx')
-
-# hcot_bl_long = biochemistry for long (repeated import of above)
-hcot_bl_long <- read_excel('wd/Dag 2 A-C_lab.xlsx')
+hcot_vf <- read.table('./ss/HCOT_DXA VF_A-C.txt', sep=",", header=TRUE)
 
 # hcot_wb = whole body composition dexa data
-hcot_wb <- read.table('wd/HCOT_DXA Whole_A-C.txt', sep=",", header=TRUE) 
+hcot_wb <- read.table('./ss/HCOT_DXA Whole_A-C.txt', sep=",", header=TRUE) 
+
+# hcot_lf = liver fat data from MRI
+hcot_lf <- read.table('./ss/HCOT_MRI_A-C_long.txt', sep=",", header=TRUE)
 
 # hcot_vm = vo2max data
-hcot_vm <- read.table('wd/HCOT_VO2max_A-C.txt', sep=",", header=TRUE)
+hcot_vm <- read.table('./ss/HCOT_VO2max_A-C.txt', sep=",", header=TRUE)
+
+# hcot_bl = biochemistry for baseline
+hcot_bl <- read.table('./ss/HCOT_lab_A-C.txt', sep=",", header=T, check.names = F, na.strings = c("", " ")) 
+# check.names=F when importing from txt due to +/- in headers
+
+# hcot_mr = VAT from MRI
+hcot_mr <- read.table('./ss/VAT_MRI.txt', sep="\t", header=T)
 
 
-	### WRANGLE DATA ###
-
+### WRANGLE DATA ###
 # ANTHROPOMETRY
 hcot_ap <- hcot_ap %>%
   select(-c(takenekg_day1:takenfood_day1)) %>% # remove vars that are just checkboxes
@@ -63,25 +66,10 @@ hcot_ap <- hcot_ap %>%
   mutate(id = str_pad(id, 3, pad="0")) %>% # make id 3 chr
   mutate(id = as.character(id)) # make id chr
 
-# data structure
-str(hcot_ap)
-
-
 # BIRTH WEIGHT
 hcot_bw <- hcot_bw %>%
   mutate(id = str_pad(id, 3, pad="0")) %>% # make id 3 chr
   mutate(id = as.character(id)) # make id chr
-
-# data structure
-str(hcot_bw)
-
-# MERGE
-# hcot = main df
-hcot <- left_join(hcot_ap, hcot_bw, by="id")
-
-# data structure 
-str(hcot)
-
 
 # TRAINING RANDOMIZATION
 hcot_tr <- hcot_tr %>%
@@ -93,17 +81,6 @@ hcot_tr <- hcot_tr %>%
                                       "Training"=c("Træning","træning","Træning ","træning "), 
                                       "Control"=c("Kontrol ","kontrol ")))
 
-# data str
-str(hcot_tr)
-
-# MERGE
-# merge into main df
-hcot <- left_join(hcot, hcot_tr, by="id") 
-
-# df str
-str(hcot)
-
-
 # DEXA DATA
 hcot_bd <- hcot_bd %>%
   select(Patient.ID, Total.BMD, dexavis) %>% # select only relevant vars
@@ -114,9 +91,6 @@ hcot_bd <- hcot_bd %>%
   mutate(visit = fct_recode(visit, "M"="Midtvejs")) %>% # recode midway visit to be consistent with one-chr naming convention
   mutate(visitid = paste(visit,id, sep="")) # create a visit id concat var
 
-# df str
-str(hcot_bd)
-
 hcot_vf <- hcot_vf %>%
   select(VAT.masse, VAT.volumen, dexavis, Patient.ID) %>% # select only relevant vars
   rename(id = Patient.ID, visit = dexavis, vat_mass = VAT.masse, vat_volume = VAT.volumen) %>% # rename vars for consistency
@@ -125,15 +99,10 @@ hcot_vf <- hcot_vf %>%
   mutate(visit = fct_recode(visit, "M"="Midtvejs")) %>% # recode midway visit
   mutate(visitid = paste(visit,id, sep="")) # concat visit + id
 
-str(hcot_vf)
-
 hcot_wb <- hcot_wb %>%
   mutate(android_tissuefatpercentage = Android.Fedtmasse/Android.Vævsmasse*100) %>% # calc andriod tissue fat %
   mutate(gynoid_tissuefatpercentage = Gynoid.Fedtmasse/Gynoid.Vævsmasse*100) %>% # calc gynoid tissue fat %
   mutate(AG_fatratio = Android.Fedtmasse/Gynoid.Fedtmasse) %>% # calc android/gynoid fat mass ratio
-  mutate(trunktotrat = Krop.Fedtmasse/Total.Fedtmasse) %>%
-  mutate(legtotrat = Ben.Fedtmasse/Total.Fedtmasse) %>%
-  mutate(legtrunkrat = Krop.Fedtmasse/Ben.Fedtmasse) %>%
   select( # select relevant vars
     Patient.ID, 
     dexavis, 
@@ -142,10 +111,7 @@ hcot_wb <- hcot_wb %>%
     gynoid_tissuefatpercentage, 
     Total.Fedtmasse, 
     Total.Fedtfri.masse_excl_bone, 
-    Total.Væv..Fedt,
-    trunktotrat,
-    legtotrat,
-    legtrunkrat) %>%
+    Total.Væv..Fedt) %>%
   rename( # rename for consistensy and comprehension
     id = Patient.ID, 
     visit = dexavis, 
@@ -159,35 +125,11 @@ hcot_wb <- hcot_wb %>%
   mutate(visit = fct_recode(visit, "M"="Midtvejs")) %>% # recode midway visit
   mutate(visitid = paste(visit,id, sep="")) # concat visit + id
 
-# df str
-str(hcot_wb)
-
 # join dexa data
 hcot_dx <- full_join(hcot_bd,hcot_vf, by=c("id","visit","visitid"))
 hcot_dx <- full_join(hcot_dx,hcot_wb, by=c("id","visit","visitid"))
 
-# df str
-str(hcot_dx)
-
-nrow(hcot_dx) # number of obs in dexa data
-sum(hcot_dx$visit=='M') #number of midway visits
-nrow(hcot_dx) - sum(hcot_dx$visit=='M') # expected number of obs if midway are removed
-
-# Only run this if you want midway visits removed
-hcot_dx <- hcot_dx %>%
-  filter(visit != "M") %>%
-  droplevels()
-
-# df str
-str(hcot_dx)
-
-# MERGE
-# join
-hcot <- full_join(hcot, hcot_dx, by=c("id","visit","visitid"))
-
-str(hcot)
-
-# LIVERFAT
+# MRI LIVER
 hcot_lf <- hcot_lf %>%
   select(-Id1) %>% # remove Id1 (løbenummer)
   rename(liverfatpercent = Percent_fat) %>% # rename var for comprehension
@@ -195,17 +137,7 @@ hcot_lf <- hcot_lf %>%
   mutate(id = as.character(id)) %>% # id as chr
   mutate(visitid = paste(visit,id, sep="")) # concat visit + id
 
-# df str
-str(hcot_lf)
-
-# MERGE
-hcot <- left_join(hcot, hcot_lf, by=c("id","visit","visitid"))
-
-#df str
-str(hcot)
-
-
-# VO2 MAX TESTS
+# VO2MAX DATA
 hcot_vm <- hcot_vm %>%
   select(ID, VO2_20sec_max, VCO2_20sec_max, RQ_20sec_max, fitness_lvl, mocvis) %>% # rm unnecessary vars
   rename(id = ID, visit = mocvis) %>% # rename vars to something comprehensible 
@@ -214,31 +146,7 @@ hcot_vm <- hcot_vm %>%
   mutate(visit = fct_recode(visit, "M"="Midtvejs")) %>% # consistency
   mutate(visitid = paste(visit,id, sep="")) # concat vis id
 
-# df str
-str(hcot_vm)
-
-nrow(hcot_vm) # number of obs in vm data
-sum(hcot_vm$visit=='M') #number of midway visits
-nrow(hcot_vm) - sum(hcot_vm$visit=='M') # expected number of obs if midway are removed
-
-# midway visits removed, only run if you want them removed
-hcot_vm <- hcot_vm %>%
-  filter(visit != "M") %>%
-  droplevels()
-
-# df str
-str(hcot_vm)
-
-# MERGE
-hcot <- full_join(hcot, hcot_vm, by=c("id","visit","visitid"))
-
-# df str
-str(hcot)
-
-
-# BLOOD SAMPLES
-# hcot_bl only contains baseline blood samples, not samples from the MMTT
-# See below for MMTT results
+# BIOCHEMISTRY
 hcot_bl <- hcot_bl %>%
   mutate(`ins-127_day2` = gsub("<7", 3.5, `ins-127_day2`)) %>% # rm limits of detection, replace by half of LoD
   mutate(`ins-126_day2` = gsub("<7", 3.5, `ins-126_day2`)) %>% 
@@ -251,60 +159,65 @@ hcot_bl <- hcot_bl %>%
   mutate(`ins-125_day2` = as.numeric(`ins-125_day2`)) %>%
   mutate(`cpep-127_day2` = as.numeric(`cpep-127_day2`)) %>%
   mutate(`cpep-126_day2` = as.numeric(`cpep-126_day2`)) %>%
-  mutate(`cpep-125_day2` = as.numeric(`cpep-125_day2`)) %>%
-  rowwise() %>%
-  mutate(bas_glu = mean(c(`glu-127_day2`, `glu-126_day2`, `glu-125_day2`))) %>% # calc means of three repeated measurements
-  mutate(bas_ins = mean(c(`ins-127_day2`, `ins-126_day2`, `ins-125_day2`))) %>% 
-  mutate(bas_cpe = mean(c(`cpep-127_day2`, `cpep-126_day2`, `cpep-125_day2`)))
+  mutate(`cpep-125_day2` = as.numeric(`cpep-125_day2`))
 
+# since rowwise ops causes downstream problems: 
+# sep rpt measures, transform to long, then to tidy, and merge back into org data:
+srm.hcot_bl <- hcot_bl %>%
+  select(visitid, 
+         `glu-127_day2`, `glu-126_day2`, `glu-125_day2`,
+         `ins-127_day2`, `ins-126_day2`, `ins-125_day2`,
+         `cpep-127_day2`, `cpep-126_day2`, `cpep-125_day2`) %>%
+  gather(var, val, -visitid) %>%
+  separate(var, c("var","time"), "-") %>%
+  spread(var, val) %>%
+  group_by(visitid) %>%
+  summarise(
+    bas_glu = mean(glu, na.rm=T),
+    bas_ins = mean(ins, na.rm=T),
+    bas_cpe = mean(cpep, na.rm=T)
+  ) 
+hcot_bl <- full_join(hcot_bl, srm.hcot_bl, by="visitid")
+
+# get vars that are relevant for a baseline biochem dataset, amend as needed
 hcot_bl <- hcot_bl %>%
-  select(visit, id, bas_glu, bas_ins, bas_cpe, a1c_day2, cholesterol_day2, hdl_day2, ldl_day2, triglyc_day2, alat_day2
-         , asat_day2, ggt_day2, factiiviix_day2) %>% # sel rlv vars
+  select(visit, id, visitid, bas_glu, bas_ins, bas_cpe, a1c_day2, cholesterol_day2, hdl_day2, ldl_day2, triglyc_day2, alat_day2, 
+         asat_day2, ggt_day2, factiiviix_day2) %>% # sel rlv vars
   mutate(id = str_pad(id, 3, pad="0")) %>% # pad id
   mutate(id = as.character(id)) %>% # id as chr
-  mutate(visitid = paste(visit,id, sep="")) %>% # concat id vis
   mutate(homa_ir = (bas_glu*bas_ins)/22.5) # calc homa
 
-# df str
-str(hcot_bl)
+# for long format blood samples (i.e. repeated c-pep etc. measures)
+hcot_mt <- read.table('./ss/HCOT_lab_A-C.txt', sep=",", header=T, check.names = F, na.strings = c("", " ")) 
+hcot_mt <- hcot_mt %>%
+  select(matches('glu|cpep|ins'), id, visitid, visit, -glufroma1c_day2)
 
-hcot <- full_join(hcot, hcot_bl, by=c("id","visit","visitid"))
+colnames(hcot_mt) <- gsub("_day2", "", colnames(hcot_mt))
+colnames(hcot_mt) <- gsub("cpep", "cpe", colnames(hcot_mt))
 
-# df str
-str(hcot)
+hcot_mt <- hcot_mt %>% 
+  gather(var, val, `glu-127`:`cpe+300`)
 
+hcot_mt$time <- substr(hcot_mt$var, 4,9)
+hcot_mt$var <- substr(hcot_mt$var, 1,3)
 
-# MMTT
-# for long format blood samples (i.e. repeated c-pep etc. measures) from the MMTT
-hcot_bl_long <- hcot_bl_long %>%
-  select(matches('glu|cpep|ins'), id, visitid, visit, -glufroma1c_day2) # select only glu, cpep, ins
+hcot_mt$time <- as.numeric(hcot_mt$time)
 
-colnames(hcot_bl_long) <- gsub("_day2", "", colnames(hcot_bl_long)) # remove suffix
-colnames(hcot_bl_long) <- gsub("cpep", "cpe", colnames(hcot_bl_long)) # rename cpep to cpe because i like it better
-
-hcot_bl_long <- hcot_bl_long %>% 
-  gather(var, val, `glu-127`:`cpe+300`) # transform from wide to long
-
-hcot_bl_long$time <- substr(hcot_bl_long$var, 4,9) #extract time from var string
-hcot_bl_long$var <- substr(hcot_bl_long$var, 1,3) #extract var (glu|cpep|ins) from var string (replaces)
-
-hcot_bl_long$time <- as.numeric(hcot_bl_long$time) # time as numeric
-
-hcot_bl_long <- hcot_bl_long %>%
-  mutate(val = gsub("<7", 3.5, val)) %>% # rm limits of detection, replace by half of LoD 
+hcot_mt <- hcot_mt %>%
+  mutate(val = gsub("<7", 3.5, val)) %>%
   mutate(val = gsub("<50", 25, val)) 
 
-hcot_bl_long$val <- as.numeric(hcot_bl_long$val) # val as numeric
+hcot_mt$val <- as.numeric(hcot_mt$val)
 
-str(hcot_bl_long)
+hcot_mt <- hcot_mt %>%
+  mutate(id = str_pad(id, 3, pad="0")) %>% # pad id
+  mutate(id = as.character(id)) # id as chr
 
-hcot_bl_halflong <- hcot_bl_long %>% #Transform to dataset where glu|cpep|ins are each own variable
+hcot_mt <- hcot_mt %>%
   spread(var, val)
 
-str(hcot_bl_halflong)
-
 # In order for aggregate results from MMTT to be put into "baseline" dataset, calculate AUCs
-auc_a <- hcot_bl_halflong %>%
+auc_a <- hcot_mt %>%
   filter(visit=="A") %>%
   group_by(id) %>%
   summarize(AUC_glu = auc(time, glu, type = "spline"),
@@ -312,15 +225,15 @@ auc_a <- hcot_bl_halflong %>%
             AUC_ins = auc(time, ins, type = "spline")) %>%
   mutate(visit = "A") %>%
   mutate(visitid = paste(visit,id, sep="")) # concat id vis
-auc_b <- hcot_bl_halflong %>%
+auc_b <- hcot_mt %>%
   filter(visit=="B") %>%
   group_by(id) %>%
   summarize(AUC_glu = auc(time, glu, type = "spline"),
             AUC_cpe = auc(time, cpe, type = "spline"),
             AUC_ins = auc(time, ins, type = "spline")) %>%
   mutate(visit = "B") %>%
-mutate(visitid = paste(visit,id, sep="")) # concat id vis
-auc_c <- hcot_bl_halflong %>%
+  mutate(visitid = paste(visit,id, sep="")) # concat id vis
+auc_c <- hcot_mt %>%
   filter(visit=="C") %>%
   group_by(id) %>%
   summarize(AUC_glu = auc(time, glu, type = "spline"),
@@ -330,8 +243,61 @@ auc_c <- hcot_bl_halflong %>%
   mutate(visitid = paste(visit,id, sep="")) # concat id vis
 auc <- rbind(auc_a, auc_b, auc_c)
 
-# df str
-str(auc)
+# MRI VAT
+hcot_mr <- hcot_mr %>%
+  select(id, visit, mri_vat) %>%
+  mutate(id = str_pad(id, 3, pad="0")) %>% # pad id
+  mutate(id = as.character(id)) %>% # id as chr
+  mutate(visitid = paste(visit,id, sep="")) # concat vis id
 
-# MERGE
+
+### MERGE STUFF ###
+# dexa + anthropometry
+hcot <- full_join(hcot_ap, hcot_dx, by=c("id","visit","visitid"))
+dim(hcot) 
+
+#liver fat
+hcot <- left_join(hcot, hcot_lf, by=c("id","visit","visitid"))
+dim(hcot) 
+
+#vo2max
+hcot <- full_join(hcot, hcot_vm, by=c("id","visit","visitid"))
+dim(hcot) 
+
+#biochem (only wideformat)
+hcot <- full_join(hcot, hcot_bl, by=c("id","visit","visitid"))
+dim(hcot) 
+
+#mri
+hcot <- full_join(hcot, hcot_mr, by=c("id","visit","visitid"))
+dim(hcot) 
+
+#auc
 hcot <- full_join(hcot, auc, by=c("id","visit","visitid"))
+
+# birthweight
+hcot <- left_join(hcot, hcot_bw, by="id")
+dim(hcot) 
+
+# training randomization
+hcot <- left_join(hcot, hcot_tr, by="id") 
+dim(hcot) 
+
+# df str
+str(hcot)
+
+
+# Make a "missing obs" table
+hcot %>%
+  select(visit, id, visitid, 
+         height_day1, weight_day1, age_day1, WHR_day1, map_day1, #day 1
+         bwcat, # bw
+         randomization, # rando
+         leanmass_total, # dexa
+         liverfatpercent, # liver mri
+         fitness_lvl, # fitness test
+         bas_glu, # MT
+         mri_vat) %>% # comp mri
+  mutate_if(is.numeric, funs(replace(., .>0, 1))) %>%
+  filter(visit != "M") %>% # take out midway (lots of missing vars)
+  filter(!complete.cases(.))
